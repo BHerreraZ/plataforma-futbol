674364d1afbd249eea672bf2747d13d38fad2b47
BHerreraZ <53711258+BHerreraZ@users.noreply.github.com>
2024-05-25 21:07:55 -0400

    [FEAT] add variables with team names and rounds

--- src/config.js
+++ src/config.js
@@ -1,3 +1,24 @@
 export const colors = {
-    main: '#E2E8F0;',
-  };
+  main: '#E2E8F0;',
+};
+
+export const names = [
+  'Audax Italiano',
+  'Cobreloa',
+  'Cobresal',
+  'Colo Colo',
+  'Coquimbo Unido',
+  'Deportes Copiapó',
+  'Deportes Iquique',
+  'Everton',
+  'Huachipato',
+  'Ñublense',
+  "O'Higgins",
+  'Palestino',
+  'Universidad Católica',
+  'Universidad de Chile',
+  'Unión Española',
+  'Unión La Calera'
+];
+
+export const rounds = ['1°', '2°', '3°', '4°', '5°', '6°', '7°', '8°', '9°', '10°','11°', '12°', '13°', '14°', '15°', '16°', '17°', '18°', '19°', '20°','21°', '22°', '23°', '24°', '25°', '26°', '27°', '28°', '29°', '30°'];
